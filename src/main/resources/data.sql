8feeb3b3a3d27d341a8898549ae6a61f3b3a9d03
Michiel Thomassen <bunzzbunzz@gmail.com>
2020-03-06 23:41:53 +0100

    Add image_event_1.jpg to root Adjust image field in data.sql from null to file_read('image_event_1.jpg'), so there's an image to work with
    Add preliminary controller method to get an event by id
    
    Adjust FeignClient url to target the rest api
    Adjust getEventById parameter from String to Long
    
    Add html skeleton of event

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,2 @@
 /* values (id, enum number, description, datetime, image, location, name, price) */
-insert into event values ( 1, 0, 'test description', '2020-12-20T20:30:54', null, 'genk', 'concert', 20.00 );
+insert into event values ( 1, 0, 'test description', '2020-12-20T20:30:54', file_read('image_event_1.jpg'), 'genk', 'concert', 20.00 );
