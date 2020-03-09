8b5ef9b45b4dd4a1c419a3feac460e64cbad09d7
TomHaeldermans94 <tomhaeldermans94@gmail.com>
2020-03-09 20:29:07 +0100

    AC-Layout-01: get all events ordered by ascending date

--- src/test/resources/data-test.sql
+++ src/test/resources/data-test.sql
@@ -1,3 +1,3 @@
 /* values (id, enum number, description, datetime, image, location, name, price) */
-insert into event values ( 1, 0, 'test description', '2020-12-20T20:30:54', file_read('image_event_1.jpg'), 'genk', 'concert', 20.00 );
-insert into event values ( 2, 0, 'test description2', '2020-11-21T21:31:55', file_read('image_event_1.jpg'), 'hasselt', 'concert', 21.00 );
+insert into event values ( 1, 0, 'test description', '2020-12-20T20:30:54', file_read('image_event_1.jpg'), 'genk', 'name', 20.00 );
+insert into event values ( 2, 0, 'test description', '2020-12-20T20:30:54', file_read('image_event_1.jpg'), 'genk', 'name', 20.00 );
