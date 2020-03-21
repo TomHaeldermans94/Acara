b9c9d3aef163fff169c8b092be1570de99f4f20f
TomHaeldermans94 <tomhaeldermans94@gmail.com>
2020-03-22 00:20:35 +0100

    AC-21 add and edit events with fileupload

--- src/main/java/be/acara/events/service/EventService.java
+++ src/main/java/be/acara/events/service/EventService.java
@@ -21,44 +21,44 @@ import java.util.Map;
 
 @Service
 public class EventService {
-    
+
     private final EventRepository repository;
     private final EventMapper mapper;
     private final CategoryMapper categoryMapper;
-    
+
     @Autowired
     public EventService(EventRepository repository, EventMapper mapper, CategoryMapper categoryMapper) {
         this.repository = repository;
         this.mapper = mapper;
         this.categoryMapper = categoryMapper;
     }
-    
+
     public EventDto findById(Long id) {
         return repository.findById(id)
                 .map(mapper::map)
                 .orElseThrow(() -> new EventNotFoundException(String.format("Event with ID %d not found", id)));
     }
-    
+
     public EventList findAllByAscendingDate() {
         return new EventList(mapper.mapEntityListToDtoList(repository.findAllByOrderByEventDateAsc()));
     }
-    
+
     public CategoriesList getAllCategories() {
         return categoryMapper.map(Category.values());
     }
-    
+
     public void deleteEvent(long id) {
         Event event = getEvent(id);
         if (event.getId() == id) {
             repository.delete(event);
         }
     }
-    
+
     private Event getEvent(long id) {
         return repository.findById(id)
                 .orElseThrow(() -> new EventNotFoundException(String.format("Event with ID %d not found", id)));
     }
-    
+
     public EventDto addEvent(EventDto eventDto) {
         if (eventDto.getId() != null) {
             throw new IdAlreadyExistsException("A new entity cannot already contain an id");
@@ -76,7 +76,7 @@ public class EventService {
         Event event = mapper.map(eventDto);
         return mapper.map(repository.saveAndFlush(event));
     }
-    
+
     /**
      * This method will use the Criteria API of JPA to search with. We will use Spring Data Specification to as
      * our provider.
