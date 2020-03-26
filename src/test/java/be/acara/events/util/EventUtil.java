4f1a0069b3b8711db0aab5a438f090755b573abe
Michiel Thomassen <bunzzbunzz@gmail.com>
2020-03-26 16:42:27 +0100

    Remove UtilHelper.java Add EventUtil instead. This class contains some static helper methods.
    Remove domain tests, testing plain constructors and getters and setters isn't useful.

---
+++ src/test/java/be/acara/events/util/EventUtil.java
@@ -0,0 +1,84 @@
+package be.acara.events.util;
+
+import be.acara.events.controller.dto.EventDto;
+import be.acara.events.domain.Category;
+import be.acara.events.domain.Event;
+import be.acara.events.service.mapper.EventMapper;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+
+public class EventUtil {
+    public static final String RESOURCE_URL = "http://localhost/api/events";
+    
+    public static Event firstEvent() {
+        return Event.builder()
+                .id(1L)
+                .category(Category.MUSIC)
+                .description("event description")
+                .eventDate(LocalDateTime.now().plusYears(1L).truncatedTo(ChronoUnit.MINUTES))
+                .location("location")
+                .name("event name")
+                .price(BigDecimal.TEN)
+                .image(getImage1AsBytes())
+                .build();
+    }
+    
+    public static Event secondEvent() {
+        return Event.builder()
+                .id(2L)
+                .category(Category.THEATRE)
+                .description("another event description")
+                .eventDate(LocalDateTime.now().plusMonths(6).truncatedTo(ChronoUnit.MINUTES))
+                .location("home")
+                .name("the name of this event")
+                .price(BigDecimal.ONE)
+                .image(getImage1AsBytes())
+                .build();
+    }
+    
+    public static Event thirdEvent() {
+        return Event.builder()
+                .id(3L)
+                .category(Category.THEATRE)
+                .description("another event description")
+                .eventDate(LocalDateTime.now().plusMonths(3).truncatedTo(ChronoUnit.MINUTES))
+                .location("home")
+                .name("the name of this event")
+                .price(BigDecimal.ONE)
+                .image(getImage1AsBytes())
+                .build();
+    }
+    
+    public static EventDto map(Event event) {
+        return new EventMapper().map(event);
+    }
+    
+    public static Event map(EventDto event) {
+        return new EventMapper().map(event);
+    }
+    
+    public static List<Event> createEventListOfSize3() {
+        return List.of(
+                firstEvent(),
+                secondEvent(),
+                thirdEvent()
+        );
+    }
+    
+    public static byte[] getImage1AsBytes() {
+        try {
+            File file = new File("image_event_1.jpg");
+            FileInputStream fis = new FileInputStream(file);
+            return fis.readAllBytes();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
