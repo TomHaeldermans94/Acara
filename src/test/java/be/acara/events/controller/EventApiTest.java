63d310f4d7ab0b24bb697659d54e1eb539e10cc7
Michiel Thomassen <bunzzbunzz@gmail.com>
2020-03-27 17:18:27 +0100

    Add @AutoConfigureTestDatabase to EventApiTest.java

--- src/test/java/be/acara/events/controller/EventApiTest.java
+++ src/test/java/be/acara/events/controller/EventApiTest.java
@@ -3,12 +3,15 @@ package be.acara.events.controller;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
 public abstract class EventApiTest {
 
     @LocalServerPort
