fb07eba219842911391be7fd184559e0530e6d66
Michiel Thomassen <bunzzbunzz@gmail.com>
2020-03-28 21:14:35 +0100

    Covered all working/positive paths.
    Adjust CategoriesList to have a @NoArgsConstructor

--- src/main/java/be/acara/events/controller/dto/CategoriesList.java
+++ src/main/java/be/acara/events/controller/dto/CategoriesList.java
@@ -2,10 +2,12 @@ package be.acara.events.controller.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class CategoriesList {
     List<String> categories;
--- src/test/java/be/acara/events/controller/EventControllerTest.java
+++ src/test/java/be/acara/events/controller/EventControllerTest.java
@@ -3,24 +3,28 @@ package be.acara.events.controller;
 import be.acara.events.controller.dto.CategoriesList;
 import be.acara.events.controller.dto.EventDto;
 import be.acara.events.controller.dto.EventList;
-import be.acara.events.domain.Event;
+import be.acara.events.domain.Category;
 import be.acara.events.service.EventService;
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import static be.acara.events.util.EventUtil.*;
 import static io.restassured.module.mockmvc.RestAssuredMockMvc.given;
 import static io.restassured.module.mockmvc.RestAssuredMockMvc.standaloneSetup;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 
 @ExtendWith(MockitoExtension.class)
@@ -40,55 +44,37 @@ class EventControllerTest {
     @Test
     void findById() {
         Long id = 1L;
-        Mockito.when(eventService.findById(id)).thenReturn(map(firstEvent()));
-    
-        Event answer = given()
+        EventDto eventDto = map(firstEvent());
+        when(eventService.findById(id)).thenReturn(eventDto);
+        
+        EventDto answer = given()
                 .when()
-                    .get(RESOURCE_URL + "/" + id)
+                    .get(RESOURCE_URL + "/{id}", id)
                 .then()
+                    .log().ifError()
                     .status(HttpStatus.OK)
-                    .extract().as(Event.class);
-    
-        assertThat(answer).isEqualTo(firstEvent());
-        verifyOnce().findById(id);
-    }
-    
-    /*@Autowired
-    private MockMvc mockMvc;
-    
-    private EventController eventController;
-    
-    @Mock
-    private EventService eventService;
-    
-    @BeforeEach
-    void setUp() {
-        MockitoAnnotations.initMocks(this);
-        eventController = new EventController(eventService);
-    }
-    
-    @Test
-    void findById() {
-        Long id = 1L;
-        when(eventService.findById(id)).thenReturn(map(firstEvent()));
-    
-        ResponseEntity<EventDto> answer = eventController.findById(id);
+                    .extract().as(EventDto.class);
         
-        assertResponseEntity(answer);
-        assertEvent(answer.getBody(), map(firstEvent()));
-        
+        assertEvent(answer, eventDto);
         verifyOnce().findById(id);
     }
     
     @Test
     void findAllByAscendingDate() {
-        when(eventService.findAllByAscendingDate()).thenReturn(createEventListOfSize3());
-    
-        ResponseEntity<EventList> answer = eventController.findAllByAscendingDate();
-    
-        assertResponseEntity(answer);
-        assertEventList(answer.getBody(), createEventListOfSize3());
+        EventList eventList = createEventListOfSize3();
+        when(eventService.findAllByAscendingDate()).thenReturn(eventList);
         
+        
+        EventList answer = given()
+                .when()
+                    .get(RESOURCE_URL)
+                .then()
+                    .log().ifError()
+                    .status(HttpStatus.OK)
+                    .extract()
+                    .as(EventList.class);
+        
+        assertEventList(answer, eventList);
         verifyOnce().findAllByAscendingDate();
     }
     
@@ -96,12 +82,13 @@ class EventControllerTest {
     void deleteEvent() {
         Long id = 1L;
         doNothing().when(eventService).deleteEvent(id);
-    
-        ResponseEntity<Void> responseEntity = eventController.deleteEvent(id);
         
-        assertThat(responseEntity).isNotNull();
-        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.NO_CONTENT);
-        assertThat(responseEntity.getBody()).isNull();
+        given()
+                .when()
+                    .delete(RESOURCE_URL + "/{id}", id)
+                .then()
+                    .log().ifError()
+                    .status(HttpStatus.NO_CONTENT);
         
         verifyOnce().deleteEvent(id);
     }
@@ -115,37 +102,57 @@ class EventControllerTest {
                 )
         );
         when(eventService.getAllCategories()).thenReturn(categoriesList);
-    
-        ResponseEntity<CategoriesList> answer = eventController.findAllCategories();
         
-        assertResponseEntity(answer);
-        assertCategoriesList(answer.getBody(), categoriesList);
-    
+        CategoriesList answer = given()
+                .when()
+                    .get(RESOURCE_URL + "/categories")
+                .then()
+                    .log().ifError()
+                    .contentType(ContentType.JSON)
+                    .status(HttpStatus.OK)
+                    .extract().as(CategoriesList.class);
+        
+        assertCategoriesList(answer,categoriesList);
+        
         verifyOnce().getAllCategories();
     }
     
     @Test
     void addEvent() {
-        when(eventService.addEvent(map(firstEvent()))).thenReturn(map(firstEvent()));
+        EventDto eventDto = map(firstEvent());
+        when(eventService.addEvent(eventDto)).thenReturn(eventDto);
     
-        ResponseEntity<EventDto> answer = eventController.addEvent(map(firstEvent()));
-        
-        assertResponseEntity(answer, HttpStatus.CREATED);
-        assertThat(answer.getHeaders().getLocation()).isEqualTo(URI.create("/api/events/1"));
-        assertEvent(answer.getBody(),map(firstEvent()));
-        
-        verifyOnce().addEvent(map(firstEvent()));
+        EventDto answer = given()
+                    .body(eventDto)
+                    .contentType(ContentType.JSON)
+                .when()
+                    .post(RESOURCE_URL)
+                .then()
+                    .log().ifError()
+                    .status(HttpStatus.CREATED)
+                    .contentType(ContentType.JSON)
+                    .extract().as(EventDto.class);
+    
+        assertEvent(answer, eventDto);
+        verifyOnce().addEvent(eventDto);
     }
     
     @Test
     void searchEvent() {
+        Map<String, String> searchParams = new HashMap<>();
         when(eventService.search(anyMap())).thenReturn(new EventList());
     
-        ResponseEntity<EventList> answer = eventController.search(Collections.emptyMap());
+        EventList answer = given()
+                    .params(searchParams)
+                .when()
+                    .get(RESOURCE_URL + "/search")
+                .then()
+                    .log().ifError()
+                    .contentType(ContentType.JSON)
+                    .status(HttpStatus.OK)
+                    .extract().as(EventList.class);
         
-        assertResponseEntity(answer);
-        assertEventList(answer.getBody(),new EventList());
-        
+        assertEventList(answer, new EventList());
         verifyOnce().search(Collections.emptyMap());
     }
     
@@ -153,14 +160,20 @@ class EventControllerTest {
     void editEvent() {
         EventDto event = map(firstEvent());
         when(eventService.editEvent(event.getId(), event)).thenReturn(event);
+    
+        EventDto answer = given()
+                    .body(event)
+                    .contentType(ContentType.JSON)
+                .when()
+                    .put(RESOURCE_URL + "/{id}", event.getId())
+                .then()
+                    .log().ifError()
+                    .status(HttpStatus.OK)
+                    .extract().as(EventDto.class);
         
-        ResponseEntity<EventDto> answer = eventController.editEvent(event.getId(), event);
-        
-        assertResponseEntity(answer);
-        assertEvent(answer.getBody(), event);
-        
+        assertEvent(answer, event);
         verifyOnce().editEvent(firstEvent().getId(), event);
-    }*/
+    }
     
     private void assertEventList(EventList response, EventList expected) {
         assertThat(response).isNotNull();
@@ -177,10 +190,6 @@ class EventControllerTest {
         assertThat(answer.getStatusCode()).isEqualTo(httpStatus);
         assertThat(answer.getStatusCodeValue()).isEqualTo(httpStatus.value());
         assertThat(answer.getBody()).isNotNull();
-    }
-    
-    private void assertResponseEntity(ResponseEntity<?> answer) {
-        assertResponseEntity(answer, HttpStatus.OK);
     }
     
     private void assertEvent(EventDto response, EventDto expected) {
