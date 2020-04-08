d9acaf55a12954a380a45cfe415b0a5fc925568d
Michiel Thomassen <bunzzbunzz@gmail.com>
2020-04-08 16:54:24 +0200

    Adjusted data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -27,11 +27,11 @@ insert into event values ( 24, 'Theatre', 'GlueX', '2020-09-27T00:00:00', file_r
 insert into event values ( 25, 'Theatre', 'CodeCrafts', '2020-10-01T00:00:00', file_read('image_event_1.jpg'), 'Vienna', 'CodeCrafts', 79.00 );
 
 /* gebruik hier de online bcrypt encoder! https://bcrypt-generator.com/*/
-insert into user values (1,'adminFirst','adminLast','$2y$10$14ux7BShikxkMJ2.qVyXOOIur8Q0Br58RzfdxYpC9k5DqEXqzEdx.','admin');
-insert into user values (2,'Ruben','Neven','rubenpw','ruben');
-insert into user values (3,'Tom','Haeldermans','tompw','tom');
-insert into user values (4,'Michiel','Thomassen','$2a$09$NXc2Hlu9ng/rcjMHa4ggZO5J6ZXjZHtQZC4Ar360cJNIMVSW28K42','michiel');
-insert into user values (5,'test','test','$2y$10$mUDfNQFM4HXQzgW98NXD6eS.CxHjVkRq2E20ZT.0YM2zJu1AsJblm','test');
+insert into user values (1,'adminFirst','adminLast','$2y$10$14ux7BShikxkMJ2.qVyXOOIur8Q0Br58RzfdxYpC9k5DqEXqzEdx.','admin'); /* admin admin */
+insert into user values (2,'Ruben','Neven','$2y$10$7D8fk.KuQGuTjxsYOcAnUePD.eph.fSJyGOOZ3DJuNy0l2OBL39JS','ruben'); /* ruben rubenpw */
+insert into user values (3,'Tom','Haeldermans','$2y$10$r9vGBv4yyCGlr6mhfENLf.QVaPiPtKf2CL2aQhsFe2KIZ6TqV.CKe','tom'); /* tom tompw */
+insert into user values (4,'Michiel','Thomassen','$2a$09$NXc2Hlu9ng/rcjMHa4ggZO5J6ZXjZHtQZC4Ar360cJNIMVSW28K42','michiel');  /* michiel michielpw */
+insert into user values (5,'test','test','$2y$10$mUDfNQFM4HXQzgW98NXD6eS.CxHjVkRq2E20ZT.0YM2zJu1AsJblm','test');  /* test test */
 
 insert into role ( id , name ) values (1, 'ROLE_ADMIN');
 insert into role ( id , name ) values (2, 'ROLE_USER');
@@ -41,6 +41,8 @@ insert into user_roles (users_id , roles_id ) values (4,1);
 insert into user_roles (users_id , roles_id ) values (4,2);
 insert into user_roles (users_id , roles_id ) values (1,1);
 insert into user_roles (users_id , roles_id ) values (1,2);
+insert into user_roles (users_id , roles_id ) values (2,1);
+insert into user_roles (users_id , roles_id ) values (3,1);
 
 insert into event_attendees values (2,2);
 insert into event_attendees values (4,2);
