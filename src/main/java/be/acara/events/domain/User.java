8a21f502ab40782ceac49e0bd58e0385a99d0a50
TomHaeldermans94 <tomhaeldermans94@gmail.com>
2020-04-13 20:43:08 +0200

    AC-12 and AC-Layout-12 edit user details

--- src/main/java/be/acara/events/domain/User.java
+++ src/main/java/be/acara/events/domain/User.java
@@ -5,6 +5,7 @@ import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
+import java.util.Objects;
 import java.util.Set;
 
 @Getter
@@ -24,6 +25,7 @@ public class User {
     @ManyToMany(mappedBy = "attendees")
     private Set<Event> events;
     @Length(min = 2, max = 30)
+    @Column(unique = true)
     private String username;
     @NotBlank
     private String password;
@@ -33,5 +35,24 @@ public class User {
     @PreRemove
     private void removeUsersFromEvents() {
         events.forEach(event -> event.getAttendees().remove(this));
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+        User user = (User) o;
+        return Objects.equals(getId(), user.getId()) &&
+                Objects.equals(getFirstName(), user.getFirstName()) &&
+                Objects.equals(getLastName(), user.getLastName()) &&
+                Objects.equals(getEvents(), user.getEvents()) &&
+                Objects.equals(getUsername(), user.getUsername()) &&
+                Objects.equals(getPassword(), user.getPassword()) &&
+                Objects.equals(getRoles(), user.getRoles());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getFirstName(), getLastName(), getEvents(), getUsername(), getPassword(), getRoles());
     }
 }
