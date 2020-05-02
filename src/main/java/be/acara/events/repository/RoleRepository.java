d4378fe937667205431dad918bc46cf7edc6c784
Michiel Thomassen <bunzzbunzz@gmail.com>
2020-05-02 23:44:04 +0200

    Remove unused imports and unneeded methods

--- src/main/java/be/acara/events/repository/RoleRepository.java
+++ src/main/java/be/acara/events/repository/RoleRepository.java
@@ -4,11 +4,8 @@ import be.acara.events.domain.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
     Role findRoleByName(String name);
-    List<Role> findByUsers_Id(Long id);
 }
