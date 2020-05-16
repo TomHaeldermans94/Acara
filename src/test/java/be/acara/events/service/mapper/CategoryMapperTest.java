c598c5a16352b2d3b38172aac4eaef4b8160fb95
Michiel Thomassen <bunzzbunzz@gmail.com>
2020-05-16 03:00:13 +0200

    100% coverage for CategoryMapper

---
+++ src/test/java/be/acara/events/service/mapper/CategoryMapperTest.java
@@ -0,0 +1,28 @@
+package be.acara.events.service.mapper;
+
+import be.acara.events.controller.dto.CategoryDto;
+import be.acara.events.domain.Category;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CategoryMapperTest {
+    
+    private CategoryMapperImpl categoryMapper;
+    
+    @BeforeEach
+    void setUp() {
+        categoryMapper = new CategoryMapperImpl();
+    }
+    
+    @Test
+    void categoryToCategoryDto() {
+        Category category = Category.MUSIC;
+        CategoryDto categoryDto = new CategoryDto(category.name(), category.getWebDisplay());
+    
+        CategoryDto answer = categoryMapper.categoryToCategoryDto(category);
+        
+        assertThat(answer).isEqualTo(categoryDto);
+    }
+}
