99862e979e7f644fb4991c573eed477665526b63
Michiel Thomassen <bunzzbunzz@gmail.com>
2020-05-19 01:58:09 +0200

    Remove NoArgsConstructor

--- src/main/java/be/acara/events/domain/CreateOrderList.java
+++ src/main/java/be/acara/events/domain/CreateOrderList.java
@@ -2,12 +2,10 @@ package be.acara.events.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 import java.util.Set;
 
 @Getter
-@NoArgsConstructor
 @AllArgsConstructor
 public class CreateOrderList {
     private Set<CreateOrder> orders;
