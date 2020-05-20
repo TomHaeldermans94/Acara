2bf282be452bee1e99e6cba14199bfaae6727720
Michiel Thomassen <bunzzbunzz@gmail.com>
2020-05-20 23:41:35 +0200

    Fix the operation not allowed error

--- src/main/java/be/acara/events/domain/CreateOrderList.java
+++ src/main/java/be/acara/events/domain/CreateOrderList.java
@@ -2,11 +2,16 @@ package be.acara.events.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import java.util.HashSet;
 import java.util.Set;
 
 @Getter
+@Setter
+@NoArgsConstructor
 @AllArgsConstructor
 public class CreateOrderList {
-    private Set<CreateOrder> orders;
+    private Set<CreateOrder> orders = new HashSet<>();
 }
