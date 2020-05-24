d2480d1455636e2fc0ea7488bae5472a128907d4
Michiel Thomassen <bunzzbunzz@gmail.com>
2020-05-25 00:25:06 +0200

    Add documentation to EventController

--- src/main/java/be/acara/events/controller/EventController.java
+++ src/main/java/be/acara/events/controller/EventController.java
@@ -30,7 +30,7 @@ public class EventController {
     private final UserService userService;
     private final EventMapper eventMapper;
     private final CategoryMapper categoryMapper;
-
+    
     @Autowired
     public EventController(EventService eventService, UserService userService, EventMapper eventMapper, CategoryMapper categoryMapper) {
         this.eventService = eventService;
@@ -38,7 +38,15 @@ public class EventController {
         this.eventMapper = eventMapper;
         this.categoryMapper = categoryMapper;
     }
-
+    
+    /**
+     * Finds an event with the specified ID.
+     * <p>
+     * The returned DTO will be enriched with related events and if the event is liked by this user.
+     *
+     * @param eventId the id of the event to find.
+     * @return a ResponseEntity of type EventDto
+     */
     @GetMapping("/{id}")
     public ResponseEntity<EventDto> findById(@PathVariable("id") Long eventId) {
         Event event = eventService.findById(eventId);
@@ -49,7 +57,16 @@ public class EventController {
         enrichEventDtoWithLikes(relatedEvents);
         return ResponseEntity.ok(eventDto);
     }
-
+    
+    /**
+     * Gets all events. Default sorted by ascending date.
+     * <p>
+     * Pass parameters like ?name="event" to find all events with event in it's name.
+     *
+     * @param params   a map of parameters containing possible filtering data
+     * @param pageable the paging and sorting parameters
+     * @return a ResponseEntity of type EventList
+     */
     @GetMapping()
     public ResponseEntity<EventList> findAllByAscendingDate(@RequestParam Map<String, String> params, Pageable pageable) {
         Page<Event> eventPage = eventService.findAll(params, pageable);
@@ -71,7 +88,7 @@ public class EventController {
         eventList.setNextAttendingEvents(eventMapper.eventListToEventDtoList(nextEvents));
         enrichEventDtoWithLikes(eventList.getNextAttendingEvents());
     }
-
+    
     private void enrichEventDtoWithLikes(Collection<EventDto> eventDtos) {
         User user = userService.getCurrentUser();
         if (user != null) {
@@ -79,20 +96,37 @@ public class EventController {
             eventDtos.forEach(eventDto -> eventDto.setLiked(ids.contains(eventDto.getId())));
         }
     }
-
+    
+    /**
+     * Deletes an event from the service
+     *
+     * @param id the id of the event to delete
+     * @return a ResponseEntity of type Void.
+     */
     @DeleteMapping("/{id}")
     @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<Void> deleteEvent(@PathVariable("id") Long id) {
         eventService.deleteEvent(id);
         return ResponseEntity.noContent().build();
     }
-
+    
+    /**
+     * Gets all categories
+     *
+     * @return A ResponseEntity of type CategoriesList
+     */
     @GetMapping("/categories")
     public ResponseEntity<CategoriesList> findAllCategories() {
         List<CategoryDto> categoryDtos = eventService.getAllCategories().stream().map(categoryMapper::categoryToCategoryDto).collect(Collectors.toList());
         return ResponseEntity.ok(new CategoriesList(categoryDtos));
     }
-
+    
+    /**
+     * Adds an event to the service
+     *
+     * @param eventDto the event to create
+     * @return a ResponseEntity of type EventDto containing the newly created event
+     */
     @PostMapping
     @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<EventDto> addEvent(@RequestBody @Valid EventDto eventDto) {
@@ -100,19 +134,40 @@ public class EventController {
         URI uri = URI.create(String.format("/api/events/%d", event.getId()));
         return ResponseEntity.created(uri).body(eventMapper.eventToEventDto(event));
     }
-
+    
+    /**
+     * Edits an event
+     *
+     * @param id       the id of the event to edit
+     * @param eventDto the new body of the event to edit
+     * @return a ResponseEntity of type EventDto with the edited body
+     */
     @PutMapping("/{id}")
     @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<EventDto> editEvent(@PathVariable("id") Long id, @RequestBody @Valid EventDto eventDto) {
         Event event = eventService.editEvent(id, eventMapper.eventDtoToEvent(eventDto));
         return ResponseEntity.ok(eventMapper.eventToEventDto(event));
     }
-
+    
+    /**
+     * Finds all events that the specified user id has subscribed to.
+     *
+     * @param id       the id of the user
+     * @param pageable pageable params
+     * @return a ResponseEntity of type EventList containing all the subscribed events, matching the pageable position
+     */
     @GetMapping("/userevents/{id}")
     public ResponseEntity<EventList> findEventsByUserId(@PathVariable("id") Long id, Pageable pageable) {
         return ResponseEntity.ok(eventMapper.pageToEventList(eventService.findEventsByUserId(id, pageable)));
     }
-
+    
+    /**
+     * Finds all events that the specified user id has liked.
+     *
+     * @param id       the id of the user
+     * @param pageable pageable params
+     * @return a ResponseEntity of type EventList containing all the liked events, matching the pageable position
+     */
     @GetMapping("/likedevents/{id}")
     public ResponseEntity<EventList> findLikedEventsByUserId(@PathVariable("id") Long id, Pageable pageable) {
         return ResponseEntity.ok(eventMapper.pageToEventList(eventService.findLikedEventsByUserId(id, pageable)));
