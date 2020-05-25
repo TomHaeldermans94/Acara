5d2cff65fcc82ced208fb54723d58fb1fc4d2bd5
Michiel Thomassen <bunzzbunzz@gmail.com>
2020-05-25 16:51:23 +0200

    Add documentation to domain package classes

--- src/main/java/be/acara/events/domain/Role.java
+++ src/main/java/be/acara/events/domain/Role.java
@@ -6,14 +6,26 @@ import lombok.NoArgsConstructor;
 import javax.persistence.*;
 import java.util.Set;
 
+/**
+ * A role class to allow permissions for an user
+ */
 @Entity
 @Data
 @NoArgsConstructor
 public class Role {
+    /**
+     * The id of this role
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    /**
+     * The name of the role
+     */
     private String name;
+    /**
+     * The users that have this role
+     */
     @ManyToMany(mappedBy = "roles")
     private Set<User> users;
 }
--- src/main/java/be/acara/events/domain/converter/CategoryConverter.java
+++ src/main/java/be/acara/events/domain/converter/CategoryConverter.java
@@ -5,13 +5,28 @@ import be.acara.events.domain.Category;
 import javax.persistence.AttributeConverter;
 import javax.persistence.Converter;
 
+/**
+ * Converts a Category Enum to a String and vice-versa.
+ */
 @Converter
 public class CategoryConverter implements AttributeConverter<Category, String> {
+    /**
+     * Converts a Category enum to a string
+     *
+     * @param category the category enum value to convert to string
+     * @return the string value of the provided enum
+     */
     @Override
     public String convertToDatabaseColumn(Category category) {
         return category.getWebDisplay();
     }
-
+    
+    /**
+     * Converts a string value to a Category Enum
+     *
+     * @param name the category name string
+     * @return the Category Enum that matches the provided string
+     */
     @Override
     public Category convertToEntityAttribute(String name) {
         return Category.valueOf(name.toUpperCase());
