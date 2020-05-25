f5bac8f0600ebd5c98ad63e0f54bf212bc31f275
TomHaeldermans94 <tomhaeldermans94@gmail.com>
2020-05-25 20:57:31 +0200

    doc04&05

--- src/main/java/be/acara/events/service/mail/MailService.java
+++ src/main/java/be/acara/events/service/mail/MailService.java
@@ -1,11 +1,13 @@
 package be.acara.events.service.mail;
 
 import be.acara.events.domain.CreateOrderList;
-import be.acara.events.domain.Event;
 import be.acara.events.domain.User;
 
-import java.util.List;
-
 public interface MailService {
+    /**
+     * sends an email to the user with the created ticket (pdf) in attachment
+     * @param createOrderList the list of orders of which a ticket has to be made
+     * @param user the user that bought the order
+     */
     void sendMessageWithAttachment(CreateOrderList createOrderList, User user);
 }
--- src/main/java/be/acara/events/service/pdf/PdfServiceImpl.java
+++ src/main/java/be/acara/events/service/pdf/PdfServiceImpl.java
@@ -1,9 +1,6 @@
 package be.acara.events.service.pdf;
 
-import be.acara.events.domain.CreateOrder;
-import be.acara.events.domain.CreateOrderList;
-import be.acara.events.domain.Event;
-import be.acara.events.domain.User;
+import be.acara.events.domain.*;
 import be.acara.events.exceptions.PdfException;
 import be.acara.events.service.EventService;
 import com.lowagie.text.*;
@@ -17,6 +14,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.UUID;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
