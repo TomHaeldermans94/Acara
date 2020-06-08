33ae734d8e99239d3f4b16b4b98215e468194807
Michiel Thomassen <bunzzbunzz@gmail.com>
2020-06-08 15:14:37 +0200

    Replace custom loggers by the loggers from lombok

--- src/main/java/be/acara/events/service/pdf/HeaderFooter.java
+++ src/main/java/be/acara/events/service/pdf/HeaderFooter.java
@@ -8,16 +8,14 @@ import com.lowagie.text.pdf.PdfPCell;
 import com.lowagie.text.pdf.PdfPTable;
 import com.lowagie.text.pdf.PdfPageEventHelper;
 import com.lowagie.text.pdf.PdfWriter;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import lombok.extern.slf4j.Slf4j;
 
 import java.awt.*;
 import java.io.IOException;
 
+@Slf4j
 public class HeaderFooter extends PdfPageEventHelper {
-
-    private static final Logger logger = LogManager.getLogger(HeaderFooter.class);
-
+    
     /**
      * method that gets the direct content of the document
      *
@@ -28,7 +26,7 @@ public class HeaderFooter extends PdfPageEventHelper {
     public void onOpenDocument(PdfWriter writer, Document document) {
         writer.getDirectContent().createTemplate(30, 16);
     }
-
+    
     /**
      * method for adding the header and footer to the document
      * @param writer PdfWriter that is added to the document makes sure that every element is written to the outputstream
@@ -39,7 +37,7 @@ public class HeaderFooter extends PdfPageEventHelper {
         addHeader(writer);
         addFooter(writer);
     }
-
+    
     /**
      * private method that adds the header with the acara logo to the page
      * @param writer PdfWriter that is added to the document makes sure that every element is written to the outputstream
@@ -49,7 +47,7 @@ public class HeaderFooter extends PdfPageEventHelper {
         try {
             header.setTotalWidth(527);
             header.setLockedWidth(true);
-
+            
             // add image
             PdfPCell imageCell = new PdfPCell();
             final String imageString = "src/main/resources/images/acara-logo.jpg";
@@ -59,14 +57,14 @@ public class HeaderFooter extends PdfPageEventHelper {
             imageCell.setBorderColor(Color.LIGHT_GRAY);
             imageCell.addElement(logo);
             header.addCell(imageCell);
-
+            
             // write content
             header.writeSelectedRows(0, -1, 34, 803, writer.getDirectContent());
         } catch (DocumentException | IOException e) {
-            logger.error("Error setting header to ticket");
+            log.error("Error setting header to ticket");
         }
     }
-
+    
     /**
      * private method that adds the footer with pages to the pdf
      * @param writer PdfWriter that is added to the document makes sure that every element is written to the outputstream
@@ -81,18 +79,18 @@ public class HeaderFooter extends PdfPageEventHelper {
             footer.getDefaultCell().setFixedHeight(40);
             footer.getDefaultCell().setBorder(Rectangle.TOP);
             footer.getDefaultCell().setBorderColor(Color.LIGHT_GRAY);
-
+            
             // add copyright
             footer.addCell(new Phrase("ACARA", new Font(Font.HELVETICA, 12, Font.BOLD)));
-
+            
             // add current page count
             footer.getDefaultCell().setHorizontalAlignment(Element.ALIGN_RIGHT);
             footer.addCell(new Phrase(String.format("Page %d", writer.getPageNumber()), new Font(Font.HELVETICA, 8)));
-
+            
             // write page
             footer.writeSelectedRows(0, -1, 34, 50, writer.getDirectContent());
         } catch (DocumentException e) {
-            logger.error("Error setting the footer to the ticket");
+            log.error("Error setting the footer to the ticket");
         }
     }
 }
